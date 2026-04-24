76b3761a34816b8418be95ecc76bbd1823fcad66
P-Mohan-Sai-Chandra <pulukuri.mohansai@gmail.com>
2026-04-24 21:42:33 +0530

    Add project scaffolding, BOOKMYSHOW & modules
    Import initial project files and IDE configuration. Adds .idea workspace/modules, BOOKMYSHOW Spring Boot module (Maven wrapper, pom.xml, application class, test, resources, .gitignore, .gitattributes), and module IML files for PARKING_LOT and TIC_TAC_TOE. Also adds a parking lot schema diagram and updates .DS_Store. This appears to be an initial commit to bring multiple exercises/projects into the repo.

---
+++ BOOKMYSHOW/BOOKMYSHOW/src/test/java/com/scaler/BOOKMYSHOW/BookmyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.BOOKMYSHOW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
